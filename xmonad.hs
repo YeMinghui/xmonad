4a17a62da54f538824dc440bad090139626df5e6
YeMinghui <MH_C_Y@163.com>
2020-11-11 22:39:05 +0800

    commit

--- xmonad.hs
+++ xmonad.hs
@@ -1,5 +1,5 @@
 import qualified Data.Map as M
-import System.Exit
+import System.Exit (exitSuccess)
 
 import XMonad
 import XMonad.Actions.CopyWindow
@@ -133,10 +133,10 @@ bspLayout = avoidStruts
   $ subLayout [] Simplest
   $ BSP.emptyBSP
 
-floatLayout = avoidStruts
-  $ renamed [Replace "Float"]
-  $ windowNavigation
-  $ simplestFloat
+-- floatLayout = avoidStruts
+--   $ renamed [Replace "Float"]
+--   $ windowNavigation
+--   $ simplestFloat
 
 --twoPaneLayout = avoidStruts
   -- $ renamed [Replace "Two"]
@@ -148,8 +148,11 @@ floatLayout = avoidStruts
 
 
 myLayoutHook =
-  bspLayout ||| floatLayout ||| noBorders Full
+  bspLayout ||| noBorders Full
 
+exitHook :: IO ()
+exitHook = do
+  return ()
 -------------------------------------------------------------
 ------------------- user settings ---------------------------
 -------------------------------------------------------------
@@ -159,26 +162,27 @@ myTerminal = "alacritty"
 myScratchpadTerminal = "alacritty --class scratchpad"
 
 -- Fonts
-myFont = "xft:Source Code Pro:size=9:bold:antialias=true"
-myWideFont = "xft:Source Code Pro:size=120:bold:antialias=true"
+myFont = "xft:WenQuanYi Micro Hei:size=9:bold:antialias=true"
+myWideFont = "xft:WenQuanYi Micro Hei:size=120:bold:antialias=true"
 
 -- Rofi is unparalleled.
 myLauncherCommand = "rofi"
-myLauncherConfig = "-sort -matching fuzzy -theme Arc-Dark"
-myLauncher = myLauncherCommand ++ " " ++ myLauncherConfig ++ " -show run"
+myLauncherConfig = "-sort"
+myLauncher = myLauncherCommand ++ " " ++ myLauncherConfig ++ " -show drun"
 
 -- imitate dmenu
 myDMenu = myLauncherCommand
 myDMenuArgs = ["-dmenu", "-i"] ++ words myLauncherConfig
 
 -- Powerful yet simple to use screenshot tool
-myScreenshot = "flameshot gui -p ~/Pictures/Screenshots"
+myScreenshot = "flameshot gui"
 
--- A suckless screensaver
-myScreensaver = "slock"
+-- A lock screen
+myScreenlock = "i3lock-fancy"
 
 -- my Emacs
-myEmacsClientCommand = "emacsclient -a \"\" -c"
+myEmacsClientCommandX = "emacsclient --socket-name server  -a \"alacritty -e vim\" -c -n"
+myEmacsClientCommand = "alacritty --socket-name server -e \"emacsclient\" \"-avim\" \"-nw\""
 
 -- Workspaces
 -- The default number of workspaces and their names.
@@ -207,32 +211,30 @@ myManageHook =
     , manageDocks
     -- scratchpad where
     --   distance from left edge = 12.5%
-    --   distance from top edge  = 25%
+    --   distance from top edge  = 12.5%
     --   width                   = 75%
-    --   height                  = 50%
-    , scratchpadManageHook (W.RationalRect 0.125 0.25 0.75 0.5)
+    --   height                  = 75%
+    , scratchpadManageHook (W.RationalRect 0.125 0.125 0.75 0.75)
     ]
 
 -- Startup hook
 -- Perform an arbitrary action each time xmonad starts or is restarted.
 myStartupHook = do
+  -- set keyboard first
+  spawn "~/mhc/scripts/tools/kbd/setkbd.sh &"
+  -- run autostart script parallelly
+  spawn "~/mhc/scripts/autostart.sh &"
   ewmhDesktopsStartup
   -- Set the mouse cursor style
   setDefaultCursor xC_left_ptr
   -- HiDPI solution
   spawnOnce "xrdb -merge ~/.Xresources"
+  spawn "COLORTERM=truecolor emacs --fg-daemon=server"
   -- Screenshot app
   spawnOnce "flameshot"
-  -- Clipboard
-  spawnOnce "parcellite"
-  -- Input Method
-  spawnOnce "fcitx"
-  -- Set background wallpaper
-  spawnOnce "feh --bg-scale ~/.xmonad/background.jpg"
-  -- X compositor
-  spawnOnce "picom -cCGfF -o 0.38 -O 200 -I 200 -t 0 -l 0 -r 3 -D2 -m 0.8"
-  -- Status bar
-  spawnOnce "polybar main"
+  -- Status bar, polybar
+  spawnOnce "~/.config/polybar/launch.sh"
+  -- spawnOnce "pkill qv2ray; pkill v2ray; qv2ray"
 
 -------------------------------------------------------------
 --------------------------- mouse ---------------------------
@@ -270,7 +272,7 @@ myKeybindings =
     [("M-n", nextWS), ("M-p", prevWS), ("M-<Escape>", toggleWS)] ++
 
     -- Quit and Restart
-    [ ("M-S-q", io (exitWith ExitSuccess))
+    [ ("M-S-q", io (exitHook >> exitSuccess))
     , ("M-C-r", spawn "xmonad --recompile && xmonad --restart")
 
     -- Take a screenshot
@@ -278,18 +280,22 @@ myKeybindings =
     , ("<Print>", unGrab >> spawn myScreenshot)
 
     -- Standard programs
-    , ("M-r", spawn myLauncher)
+    , ("M-s", spawn myLauncher)
     , ("M-<Return>", spawn myTerminal)
     , ("M-S-<Return>", scratchpadSpawnActionCustom $ myScratchpadTerminal)
+    , ("M-e", spawn "~/mhc/scripts/tools/pickemoji")
 
     -- Functional keys
-    , ("<XF86AudioMute>", spawn "amixer -q set Master toggle")
-    , ("<XF86AudioLowerVolume>", spawn "amixer -q set Master 1%-")
-    , ("<XF86AudioRaiseVolume>", spawn "amixer -q set Master 1%+")
+    , ("<XF86AudioMute>", spawn "pamixer -t")
+    , ("<XF86AudioLowerVolume>", spawn "pamixer -d 10")
+    , ("<XF86AudioRaiseVolume>", spawn "pamixer -i 10")
     , ("<XF86MonBrightnessDown>", spawn "xbacklight -10")
     , ("<XF86MonBrightnessUp>", spawn "xbacklight +10")
     , ("<XF86Display>", spawn "xset dpms force standby")
-    , ("<XF86Favorites>", spawn myScreensaver)
+    , ("<XF86ScreenSaver>", spawn "xset dpms force off")
+    , ("<XF86TouchpadToggle>", spawn "~/mhc/scripts/tools/touchpad/touchpad-toggle.sh")
+    , ("M-C-S-l", spawn myScreenlock)
+    , ("M-c", spawn "xcolor | xsel -b")
 
     -- Layouts
     -- Using M-S-<Space> to reset the layout on the current workspace to default.
@@ -350,15 +356,14 @@ myKeybindings =
     -- Prompts
     , ("M-m p", passPrompt promptTheme)
     , ("M-m m", manPrompt promptTheme)
-    , ("M-m e", spawn myEmacsClientCommand)
+    , ("M-m t", spawn myEmacsClientCommand)
+    , ("M-m e", spawn myEmacsClientCommandX)
     ]
 
 
 myForbiddenKeys =
   [ "M-q"
   , "M-w"
-  , "M-e"
-  , "M-S-q"
   , "M-S-w"
   , "M-S-e"
   , "M-S-r"
